42cba15a6cca0924c9b9870bdc68c2035c9ab489
Victor Gomes <victorncg@gmail.com>
2016-03-27 00:26:21 -0400

    Update lula_popularity.R

--- lula_popularity.R
+++ lula_popularity.R
@@ -19,10 +19,13 @@ setup_twitter_oauth(consumer_key,consumer_secret,access_token,access_secret)
 
 Lula <- searchTwitter("Lula", n=100, lang="pt")
 Lula_text <- sapply(Lula, function(x) x$getText())
+
+# Removing non-alphanumeric characters
+Lula_text <- gsub(" +"," ",gsub("^ +","",gsub("[^a-zA-Z0-9 ]","",Lula_text)))
+
 Lula_corpus <- Corpus(VectorSource(Lula_text))
 
 Lula_clean <- tm_map(Lula_corpus, removePunctuation)
-
 Lula_clean <- tm_map(Lula_corpus, content_transformer(tolower))
 Lula_clean <- tm_map(Lula_corpus, removeWords,stopwords("portuguese"))
 Lula_clean <- tm_map(Lula_corpus, removeNumbers)
